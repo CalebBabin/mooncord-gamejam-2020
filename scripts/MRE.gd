c649c7c69a11ed10dbf09d135912e07a20eefbe3
Caleb <CalebBabin@users.noreply.github.com>
2020-06-01 07:23:03 -0500

    add more MRE stuff

--- scripts/MRE.gd
+++ scripts/MRE.gd
@@ -8,6 +8,12 @@ extends Node2D
 var camera:Camera2D
 var hand
 var hand_sprite
+var grasp = false
+var hand_velocity = Vector2.ZERO
+
+const HAND_ACCEL = 10
+const HAND_MAX_SPEED = 300
+const HAND_REACH = 128
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,4 +28,25 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	#hand_sprite.frame = 0
-	pass
+	var input_velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_left"):
+		input_velocity.x += -1
+	if Input.is_action_pressed("ui_right"):
+		input_velocity.x += 1
+	if Input.is_action_pressed("ui_up"):
+		input_velocity.y += -1
+	if Input.is_action_pressed("ui_down"):
+		input_velocity.y += 1
+	
+	hand_velocity = hand_velocity.move_toward(input_velocity.normalized()*HAND_MAX_SPEED, HAND_ACCEL)
+	
+	hand.position += hand_velocity*delta
+
+
+func _unhandled_input(_event:InputEvent):
+	if Input.is_action_just_pressed("ui_accept"):
+		grasp = !grasp
+		if grasp:
+			hand_sprite.frame = 1
+		else:
+			hand_sprite.frame = 0
